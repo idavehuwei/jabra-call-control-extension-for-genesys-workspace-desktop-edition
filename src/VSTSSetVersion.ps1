4e667ace9630fc1081698143f62ac6183983d675
Morten Frederiksen <mfrederiksen@jabra.com>
2017-06-21 12:03:02 +0200

    Add PowerShell build script

---
+++ src/VSTSSetVersion.ps1
@@ -0,0 +1,27 @@
+# ****************************************************************************
+# VSTSSetVersion.ps1
+# Description: Search/replace tools to update build version numbers
+# Uses BUILD_BUILDID as a build number
+# ****************************************************************************
+
+Write-Host Executing VSTSSetVersion.ps1
+
+# Stop on errors
+$ErrorActionPreference = "Stop" 
+
+$searchtext = "0\.2\.0\.0"
+$replacetext = "0.2." + $Env:BUILD_BUILDID + ".0"
+
+Write-Host Replacing $searchtext with $replacetext
+
+$files = Get-ChildItem -Recurse -include AssemblyInfo.cs
+
+foreach ($file in $files)
+{
+	Write-Host "File:" $file.FullName
+	(Get-Content $file.PSPath) |
+	Foreach-Object { $_ -replace $searchtext, $replacetext } |
+	Set-Content $file.PSPath
+}
+
+Write-Host VSTSSetVersion.ps1 execution done
